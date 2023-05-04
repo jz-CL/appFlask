cb8fe5e80e1142a487718533cd39fd86a253be18
jz-CL <janusz.zychowicz@gmail.com>
2023-05-04 02:52:30 +0200

    Szablony

--- hello.py
+++ hello.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template
-
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
@@ -12,4 +13,12 @@ def index():
 def user(name):
     return render_template('user.html', name=name)
 
-    
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
