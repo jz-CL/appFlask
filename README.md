47ecee213c8a954c097b0c28ebab22cbce8e8462
jz-CL <janusz.zychowicz@gmail.com>
2023-05-18 00:37:14 +0200

    formularze internetowe (4.0c)

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # app Flask project
 
 
+
+v4.0c
+- formularz
+- klasy formularzy
+- wbudowane walidatory
+- standardowe pola WTF - Flask-WTF
+- renderowanie formularzy HTML
+- obsługa formularzy w funkcjach widoku
+- przekierowania i sesje użytkownika
+- wyświetlanie komunikatów
+
+
 v2.0c
 - podstawowa struktura aplikacji
 
--- hello.py
+++ hello.py
@@ -1,18 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 from flask_moment import Moment
 
 from datetime import datetime
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Wygląda na to że teraz nazywasz się inaczej!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+
     return render_template('index.html', 
-                            current_time=datetime.utcnow())
+                            form=form, name=session.get('name'))
 
 
 @app.route('/user/<name>')
